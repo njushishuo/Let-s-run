f307f3e100bfb8fe815bd40dc76df910260e0244
njushishuo <675342907@qq.com>
2016-10-17 23:58:10 +0800

    finish profile

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+
+
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+});
+
