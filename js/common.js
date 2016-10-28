55d1e94e1c0920b246c9816ed7364446d165f59d
njushishuo <675342907@qq.com>
2016-10-28 22:37:02 +0800

    update

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: false, // Activate on hover
+        gutter: 20, // Spacing from edge
+        belowOrigin: true // Displays dropdown below the button
+    }
+);
--- js/user_friends.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    //随滚动识别中心内容，高亮右侧导航
-    $('.scrollspy').scrollSpy();
-
-    //对话框
-    $('.modal-trigger').leanModal();
-});
